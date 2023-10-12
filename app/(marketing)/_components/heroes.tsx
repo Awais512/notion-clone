2835c2cbc05412d5d61d5e0c4c3a220fe5e1fa48
awais512 <raza.awais25@gmail.com>
2023-10-12 23:47:44 +0100

    Landing Page

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+const Heroes = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+          <Image
+            src="/documents.png"
+            fill
+            className="object-contain"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden md:block">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain"
+            alt="Reading"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heroes;
