11b975bfbeaa5346d19843ac4a191a26af75dc65
awais512 <raza.awais25@gmail.com>
2023-10-13 15:19:23 +0100

    Spinner variants components

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,13 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
   return (
     <div
@@ -17,6 +22,17 @@ const Navbar = () => {
       <Logo />
 
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost">Log in</Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button>Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
