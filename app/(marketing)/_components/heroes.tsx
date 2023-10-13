44e678b42353e47041b2c38a8b665a946d366cfe
awais512 <raza.awais25@gmail.com>
2023-10-13 12:01:11 +0100

    Landing PAge with dark mode done

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -8,7 +8,13 @@ const Heroes = () => {
           <Image
             src="/documents.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="Documents"
+          />
+          <Image
+            src="/documents-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Documents"
           />
         </div>
@@ -16,7 +22,13 @@ const Heroes = () => {
           <Image
             src="/reading.png"
             fill
-            className="object-contain"
+            className="object-contain dark:hidden"
+            alt="Reading"
+          />
+          <Image
+            src="/reading-dark.png"
+            fill
+            className="object-contain hidden dark:block"
             alt="Reading"
           />
         </div>
