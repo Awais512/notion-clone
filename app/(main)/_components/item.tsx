d2e8abff112f39b027751b3abef7b56e3547416a
awais512 <raza.awais25@gmail.com>
2023-10-17 14:53:17 +0100

    Item Components continued

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,21 +1,70 @@
-import { LucideIcon } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import {
+  ChevronDown,
+  ChevronRight,
+  ChevronUpIcon,
+  LucideIcon,
+} from "lucide-react";
 
 interface ItemProps {
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
   label: string;
   onClick: () => void;
   icon: LucideIcon;
 }
 
-const Item: React.FC<ItemProps> = ({ label, onClick, icon: Icon }) => {
+const Item: React.FC<ItemProps> = ({
+  id,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  expanded,
+  onExpand,
+  label,
+  onClick,
+  icon: Icon,
+}) => {
+  const cheveronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary"
+      )}
     >
-      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
-      <span>{label}</span>
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          onClick={() => {}}
+        >
+          <ChevronUpIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
+        </div>
+      )}
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
+
+      <span className="truncate">{label}</span>
+      {isSearch && (
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      )}
     </div>
   );
 };
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
+import {
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings,
+} from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -131,6 +137,8 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item label="Search" icon={Search} isSearch onClick={() => {}} />
+          <Item label="Settings" icon={Settings} onClick={() => {}} />
           <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
