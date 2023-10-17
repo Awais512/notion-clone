826da4ab72b34d229e4b1087d637e270cd5091ee
awais512 <raza.awais25@gmail.com>
2023-10-17 13:46:56 +0100

    Item Components to render new items

---
+++ app/(main)/_components/item.tsx
@@ -0,0 +1,23 @@
+import { LucideIcon } from "lucide-react";
+
+interface ItemProps {
+  label: string;
+  onClick: () => void;
+  icon: LucideIcon;
+}
+
+const Item: React.FC<ItemProps> = ({ label, onClick, icon: Icon }) => {
+  return (
+    <div
+      onClick={onClick}
+      role="button"
+      style={{ paddingLeft: "12px" }}
+      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+    >
+      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      <span>{label}</span>
+    </div>
+  );
+};
+
+export default Item;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import UserItem from "./user-item";
-import { useQuery } from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import Item from "./item";
+import { toast } from "sonner";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -97,6 +100,15 @@ export const Navigation = () => {
     }
   };
 
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" });
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "failed to create a new note.",
+    });
+  };
+
   return (
     <>
       <aside
@@ -119,6 +131,7 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item onClick={handleCreate} label="New Page" icon={PlusCircle} />
         </div>
         <div className="mt-4">
           {documents?.map((document) => (
