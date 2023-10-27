1630cf963c224f9ad77655af07eabdfafdadf63c
awais512 <raza.awais25@gmail.com>
2023-10-27 02:12:49 +0100

    Remove Glitch

--- lib/edgestore.ts
+++ lib/edgestore.ts
@@ -1,5 +1,5 @@
 "use client";
-import { type EdgeStoreRouter } from "@/app/api/egdestore/[...edgestore]/route";
+import { type EdgeStoreRouter } from "@/app/api/edgestore/[...edgestore]/route";
 import { createEdgeStoreProvider } from "@edgestore/react";
 const { EdgeStoreProvider, useEdgeStore } =
   createEdgeStoreProvider<EdgeStoreRouter>();
